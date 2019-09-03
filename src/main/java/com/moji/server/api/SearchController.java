862f45fafef596173665e524a8bac9f8eb1c35c2
ywoo21 <ywooo21@gmail.com>
2019-09-04 00:03:15 +0900

    Add HashtagCourse

--- src/main/java/com/moji/server/api/SearchController.java
+++ src/main/java/com/moji/server/api/SearchController.java
@@ -1,4 +1,30 @@
 package com.moji.server.api;
 
+import com.moji.server.model.DefaultRes;
+import com.moji.server.service.SearchService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RestController
 public class SearchController {
+
+    private final SearchService searchService;
+
+    public SearchController(final SearchService searchService) {
+        this.searchService = searchService;
+    }
+
+    // 검색
+    @GetMapping("/searches")
+    public ResponseEntity<DefaultRes> search(@RequestParam final String keyword) {
+        try{
+            return new ResponseEntity<>(searchService.getSearchResultByHashtag(keyword), HttpStatus.OK);
+        } catch (Exception e){
+            log.info(e.getMessage());
+            return new ResponseEntity<>(DefaultRes.FAIL_DEFAULT_RES, HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/moji/server/service/SearchService.java
+++ src/main/java/com/moji/server/service/SearchService.java
@@ -1,4 +1,41 @@
 package com.moji.server.service;
 
+import com.moji.server.domain.Hashtag;
+import com.moji.server.model.DefaultRes;
+import com.moji.server.repository.HashtagCourseRepository;
+import com.moji.server.repository.HashtagRepository;
+import com.moji.server.repository.SearchCourseRepository;
+import com.moji.server.util.StatusCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
 public class SearchService {
+    private final HashtagRepository hashtagRepository;
+    private final HashtagCourseRepository hashtagCourseRepository;
+    private final SearchCourseRepository searchCourseRepository;
+
+    // 생성자 의존성 주입
+    public SearchService(final HashtagRepository hashtagRepository,
+                         final HashtagCourseRepository hashtagCourseRepository,
+                         final SearchCourseRepository searchCourseRepository) {
+        this.hashtagRepository = hashtagRepository;
+        this.hashtagCourseRepository = hashtagCourseRepository;
+        this.searchCourseRepository = searchCourseRepository;
+    }
+
+    public DefaultRes getSearchResultByHashtag(final String keyword){
+        Optional<List<Hashtag>> hashtag = hashtagRepository.findByTagInfoContaining(keyword);
+
+        return hashtag.map(value ->
+                DefaultRes.res(StatusCode.OK, "검색 성공",
+                        searchCourseRepository.findBy_id(
+                                hashtagCourseRepository.findBytagIdx(value.iterator().next().get_id())
+                                        .get().getCourseIdx())))
+                .orElseGet(() -> DefaultRes.res(StatusCode.NOT_FOUND, "검색 실패"));
+    }
 }
