2cc1a4dbcb3d99d9d87ad98b3d78f5698634857f
daseul bae <ektmf1993@gmail.com>
2019-10-07 13:49:16 +0900

    photo open

--- src/main/java/com/moji/server/api/BoardController.java
+++ src/main/java/com/moji/server/api/BoardController.java
@@ -28,8 +28,6 @@ public class BoardController {
                                     final HttpServletRequest httpServletRequest) {
         try {
             final int userIdx = (int) httpServletRequest.getAttribute("userIdx");
-            log.info("보드 컨트롤");
-            log.info("시작");
             return new ResponseEntity<>(boardService.saveBoard(board, userIdx), HttpStatus.OK);
         } catch (Exception e) {
             log.info(e.getMessage());
